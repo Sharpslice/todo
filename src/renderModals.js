1fda5079a0407e1123dcdae3a93b53699d320992
sharpslice <davidlein1440p@gmail.com>
2024-09-03 00:38:48 -0700

    you can now add projects

--- src/renderModals.js
+++ src/renderModals.js
@@ -24,6 +24,7 @@ export function renderNewProjectModal(){
     addBtn.textContent="add";
 
     const cancelBtn = document.createElement("button");
+    cancelBtn.type="button";
     cancelBtn.textContent="cancel";
 
     
--- src/sidebar.js
+++ src/sidebar.js
@@ -2,11 +2,14 @@ const sidebar = document.querySelector('#sideBar');
 const buttonDivs = sidebar.querySelectorAll(".button");
 import { createTodo, listOfProjects } from "./toDoFactory";
 import { renderNewProjectModal } from "./renderModals";
+import loadProjects from "./renderProjectList";
+
 
 
 
 const buttonActions ={
     'newProjectBtn': ()=> renderNewProjectModal()
+    
 }
 
 export function setUpButtonEventListeners(){
@@ -22,7 +25,15 @@ export function setUpButtonEventListeners(){
     document.addEventListener('click',(e)=>{
         if(e.target && e.target.id==="projectAddBtn")
         {
-            listOfProjects().set("test",null);
+            const projectName = document.getElementById("projectName")
+            if(listOfProjects().has(projectName.value))
+            {
+                console.log("error: project exists")
+                return;
+            }
+            listOfProjects().set(projectName.value,null);
+            loadProjects();
+
             console.log(listOfProjects());
         }
     })
