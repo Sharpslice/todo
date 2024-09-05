d7170ed140cb55e7f7788ebbf8e9185837adf9a1
sharpslice <davidlein1440p@gmail.com>
2024-09-04 18:26:27 -0700

    fixed projects being added

--- src/renderProjectList.js
+++ src/renderProjectList.js
@@ -1,13 +1,21 @@
 import { listOfProjects } from "./toDoFactory";
+import { setUpButtonEventListeners } from "./sidebar";
 
 export default function loadProjects(){
     const projects = document.getElementById("projectList");
-    
+    const temp = Array.from(document.querySelectorAll("#projectList li"));
+    const list = temp.map(item=>item.textContent);
     for(const [key,value] of listOfProjects()){
+        if(list.includes(key))
+        {
+            continue;
+        }
         const projectItem = document.createElement('li');
         projectItem.classList.add("button");
         projectItem.textContent = key;
         projects.appendChild(projectItem);
+        
+
     }
    
 }
