f0809d899c463d4b95ac7d0d4fa754444bfd9b2d
sharpslice <davidlein1440p@gmail.com>
2024-09-05 00:38:21 -0700

    task can now appear on specific projects but everything is currently hardcoded

---
+++ src/renderTaskList.js
@@ -0,0 +1,34 @@
+import { listOfProjects, addTask } from "./toDoFactory";
+
+
+
+export default function loadTaskList(projectName,task) {
+    
+    addTask(projectName,task);  
+     
+   
+    const taskList = document.getElementById("taskList")
+    const temp = Array.from(document.querySelectorAll("#taskList li"));
+    const list = temp.map(item=>item.textContent);
+    listOfProjects().get(projectName).forEach(item=>{
+        if(!list.includes(item.title))
+        {
+            const taskItem = document.createElement("li");
+            const taskDiv = document.createElement("div");
+            const title = document.createElement("span");
+            title.textContent=item.title;
+            taskDiv.appendChild(title);
+            taskItem.appendChild(taskDiv);
+            taskList.appendChild(taskItem)
+        }
+        
+        
+        // taskList.appendChild(tasks);
+
+
+    })
+
+    console.log(listOfProjects().get(projectName))
+   
+
+}
